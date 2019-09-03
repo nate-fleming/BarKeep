16afbf5ca62437caa3c16a90d700f393d70baf12
Nate Fleming <nfleming4@gmail.com>
2019-09-03 15:59:25 -0500

    initial identity setup

--- BarKeep/Data/ApplicationDbContext.cs
+++ BarKeep/Data/ApplicationDbContext.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BarKeep.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BarKeep.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) {}
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ BarKeep/Models/Cocktail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarKeep.Models
+{
+    public class Cocktail
+    {
+        [Key]
+        public int CocktailId { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        [Required]
+        public int AlcoholTypeId { get; set; }
+
+        public string Source { get; set; }
+
+        [Required]
+        public int GlasswareId { get; set; }
+
+        public string Garnish { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+
+        public virtual ICollection<Instruction> Instructions { get; set; }
+
+        public Alcoholtype AlcoholType { get; set; }
+
+        public Glassware Glassware { get; set; }
+    }
+}
